ad929104f57b1650f26855abc52457b97f23cf6d
Ronny <Ronny_xie@hotmail.com>
2017-05-05 16:26:53 +0800

    重新布局卡务首页界面

--- app/src/main/java/com/example/ronny_xie/gdcp/Fragment/card/CardActivity.java
+++ app/src/main/java/com/example/ronny_xie/gdcp/Fragment/card/CardActivity.java
@@ -2,80 +2,101 @@ package com.example.ronny_xie.gdcp.Fragment.card;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+
 import com.bumptech.glide.Glide;
 import com.example.ronny_xie.gdcp.R;
+import com.google.gson.Gson;
+
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class CardActivity extends Activity {
+public class CardActivity extends Activity implements View.OnClickListener{
+    private String html_data;
+    private Document doc;
+    private personData_Javabean personData_javabean;
+    private Handler handler;
+    private int SET_PERSON_DATA = 1001;
 
-	private ImageView image_Title;
-	private String html_data;
-	private Document doc;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.card_activity);
+        super.onCreate(savedInstanceState);
+        initBar();
+        initHandler();//设置回调
+        initData();//获取内容，布局内容
+    }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.card_activity);
-		super.onCreate(savedInstanceState);
-		initData();
-		initView();
-		LoadImageToTitle();// 获取title的图片加载入iamge
-	}
+    private void initHandler() {
+        handler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                if (msg.what == SET_PERSON_DATA) {
+                    initViewData();
+                }
+                return true;
+            }
+        });
+    }
 
-	private void initData() {
-		Thread thread = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				String data = cardClient.getPersonData(cardClient.getHttpClient());
-				Log.i("ronny", "run: "+data);
-			}
-		});
-		thread.start();
-	}
+    private void initData() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String data = cardClient.getPersonData(cardClient.getHttpClient());
+                Gson gson = new Gson();
+                personData_javabean = gson.fromJson(data, personData_Javabean.class);
+                handler.sendEmptyMessage(SET_PERSON_DATA);
+            }
+        });
+        thread.start();
+    }
 
-	private void LoadImageToTitle() {
-		Glide.with(CardActivity.this)
-				.load("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1491646149469&di=a4fd10244cf80ea61c658bd9247c316a&imgtype=0&src=http%3A%2F%2Fnews.onlinedown.net%2Fd%2Ffile%2F20160814%2F0f4c37b97c0dfb995d144e997f7a107b.jpg")
-				.into(image_Title);
-	}
+    private void LoadImageToTitle() {
+        ImageView image_Title = (ImageView) findViewById(R.id.card_activity_image);
+        Glide.with(CardActivity.this)
+                .load("http://img.hb.aicdn.com/4ae6629f4be1fd5ba818eb09e3d52afecc7fc79b42b72-Tp58cK_fw658")
+                .into(image_Title);
+    }
 
-	private void initView() {
-		image_Title = (ImageView) findViewById(R.id.card_activity_image);
-		TextView tv_name = (TextView) findViewById(R.id.fragment_card_name);
-		TextView tv_belond = (TextView) findViewById(R.id.fragment_card_belone);
-		TextView yue = (TextView) findViewById(R.id.fragment_card_money);
-		TextView kazhuangtai = (TextView) findViewById(R.id.kazhuangtai);
-		TextView dongjiezhuangtai = (TextView) findViewById(R.id.dongjiezhuangtai);
-		TextView guashizhuangtai = (TextView) findViewById(R.id.guashizhuangtai);
-//		getTitlePersonData(tv_name, tv_belond);//设置标题栏的姓名的归属
-//		getMiddleData(yue, kazhuangtai, dongjiezhuangtai, guashizhuangtai);//设置中间部分的数据
-	}
+    private void initViewData() {
+        TextView tv_name = (TextView) findViewById(R.id.fragment_card_name);
+        TextView tv_num = (TextView) findViewById(R.id.fragment_card_num);
+        TextView tv_belond = (TextView) findViewById(R.id.fragment_card_belone);
+        TextView yue = (TextView) findViewById(R.id.fragment_card_money);
+        TextView kazhuangtai = (TextView) findViewById(R.id.kazhuangtai);
+        TextView dongjiezhuangtai = (TextView) findViewById(R.id.dongjiezhuangtai);
+        tv_name.setText(personData_javabean.getName());
+        tv_num.setText("卡号："+personData_javabean.getNumber());
+        tv_belond.setText(personData_javabean.getDepartment());
+        yue.setText(personData_javabean.getBalance().replace("元",""));
+        kazhuangtai.setText(personData_javabean.getCardStatus());
+        dongjiezhuangtai.setText(personData_javabean.getFreezeStatus());
+        LoadImageToTitle();// 获取title的图片加载入iamge
+    }
 
+    private void initBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
+            // Translucent status bar
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+    }
 
-	private void getMiddleData(TextView yue, TextView kazhuangtai,TextView dongjiezhuangtai, TextView guashizhuangtai) {
-		Elements elementsByClass = doc.getElementsByClass("neiwen");
-		String card_yue = elementsByClass.get(46).text().toString();
-		card_yue = card_yue.substring(0, card_yue.indexOf("（"));
-		String card_kazhuangtai = elementsByClass.get(42).text().toString();
-		String card_dongjiezhuangtai = elementsByClass.get(44).text()
-				.toString();
-		String card_guashizhuangtai = elementsByClass.get(50).text().toString();
-		yue.setText(card_yue);
-		kazhuangtai.setText(card_kazhuangtai);
-		dongjiezhuangtai.setText(card_dongjiezhuangtai);
-		guashizhuangtai.setText(card_guashizhuangtai);
-	}
+    @Override
+    public void onClick(View v) {
 
-	private void getTitlePersonData(TextView tv_name, TextView tv_belond) {
-		Elements elementsByClass_neiwen = doc.getElementsByClass("neiwen");
-		String neiwen_name = elementsByClass_neiwen.get(1).text().toString();
-		String neiwen_belone = elementsByClass_neiwen.get(28).text().toString();
-		tv_name.setText("姓名：" + neiwen_name);
-		tv_belond.setText("所属：" + neiwen_belone);
-	}
+    }
 }
